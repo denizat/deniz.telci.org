b1d807f3ce90d503e25b3bfeb7e4cc703904ecb4
denizat <deniza.telci@gmail.com>
2021-05-14 22:31:25 -0500

    CSS constant

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 import Header from "./Header";
 import "tailwindcss/tailwind.css"
 
-// const linkCSS = "padding"
+const linkCSS = "py-1 px-2 border border-purple-700"
 
 export default () => (
   <nav className='flex flex-row'>
     <Header title="Real_Header" num={420} />
-    <Link to="/">Home</Link>
+    <Link className={linkCSS} to="/">Home</Link>
     <br />
-    <Link to="/body">Body</Link>
+    <Link className={linkCSS}to="/body">Body</Link>
     <br />
-    <Link to="/polygons">Polygons</Link>
+    <Link className={linkCSS}to="/polygons">Polygons</Link>
     <br />
-    <Link  to="/practical_react_with_ben_awad">
+    <Link  className={linkCSS}to="/practical_react_with_ben_awad">
       Practical React with Ben Awad
     </Link>
     <br />
