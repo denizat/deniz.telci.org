e003fceca3076e588b4d5ae5ac3b0c1fa7222384
denizat <deniza.telci@gmail.com>
2021-06-17 17:38:49 -0500

    finally got the bg to stick with overflow

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import Fun from "./pages/Fun"
 class App extends React.Component {
   render() {
     return (
-      <div className="bg-still-water  flex flex-col h-screen overflow-hidden   text-black font-cm ">
+      <div className="bg-still-water bg-cover  flex flex-col  overflow-auto   text-black font-cm ">
         <div className="backdrop-filter h-screen  backdrop-blur-md mx-4">
           <div className="text-6xl  p-10">Deniz Telci</div>
 
