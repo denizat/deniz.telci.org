f19d8b6f711d9336d04b7625e2383c08c89be71d
denizat <deniza.telci@gmail.com>
2021-06-25 09:12:16 -0500

    Small changes

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,21 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import "tailwindcss/tailwind.css"
 import "./index.css"
 import "./fonts/cms/cmun-serif.css"
+import Img from "./images/pic.png"
 
 import Fun from "./pages/Fun"
 import Art from "./pages/Art"
 
-class App extends React.Component {
+class App extends React.Component<{}, { imageClicked: Boolean }> {
+  constructor({ }) {
+    super(null)
+    this.state = {
+      imageClicked: false
+    }
+
+
+  }
+
   render() {
     return (
       <div className=" flex flex-col items-center  font-cm overflow-auto h-screen bg-gray-950 text-white text-xl">
@@ -28,8 +38,9 @@ class App extends React.Component {
                     </li>
                   </ul>
                 </div>
-                <div className="container mx-auto m-4">
-                  <h2 className="text-3xl">Fun Stuff:</h2>
+                <div className="flex flex-col mx-auto m-4">
+                  <div className="cursor-pointer underline" onClick={() => this.setState({ imageClicked: !this.state.imageClicked })}>{this.state.imageClicked ? <img src={Img}></img> : "Image of Me"}</div>
+                  <h2 className="text-3xl ">Fun Stuff:</h2>
                   <ul className="ml-10 list-disc">
                     <Link to="/fun" ><li>Fun</li></Link>
                     <Link to="/art" ><li>Art</li></Link>
--- src/pages/Art.tsx
+++ src/pages/Art.tsx
@@ -5,22 +5,11 @@ import "tailwindcss/tailwind.css"
 
 export default () => (
     <div className="h-full overflow-hidden ">
-        <div className="max-w-full max-h-full overflow-y-scroll text-4xl flex flex-col items-center no-scrollbar" >
+        <div className="max-w-full max-h-full text-4xl flex flex-col items-center no-scrollbar" >
             <h1>
                 Welcome to art.
             </h1>
             <Link className="text-red-700 font-bold hover:text-red-900 hover:underline animate-pulse " to="/">Please Leave</Link>
-            <div className='h-screen ' />
-            <div>
-                # Among Us is Sus<br /><br />
-
-                There is someone sus among us.<br />
-                Who will catch the imposter among us.<br />
-                They are sus.<br />
-                You seem kind of sus.<br />
-                Are you the imposter among us?<br />
-
-            </div>
         </div>
     </div>
 )
